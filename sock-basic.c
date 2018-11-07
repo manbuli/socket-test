de5c93b0fa48db4b367c55080e5336d3cb07e5dc
manbuli <372840839@qq.com>
2018-11-07 10:35:34 +0800

    init commit

---
+++ sock-basic.c
@@ -0,0 +1,18 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h> /* superset of previous */
+// man 7 ipv6
+// ipv6 - Linux IPv6 protocol implementation
+// man 7 ip
+// ip - Linux IPv4 protocol implementation
+
+// tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+// udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+// raw_socket = socket(AF_INET, SOCK_RAW, protocol);
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
